7e32d25558da109be30fcac96d59174214ec3d54
Anto Daniel <anto.daniel@gmail.com>
2015-04-07 07:42:53 +0000

    modification

--- nconf_installation.sh
+++ nconf_installation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 apt-get update
-apt-get install apache2 mysql-server mysql-client php5 libapache2-mod-php5 phpmyadmin -y
+apt-get install apache2 mysql-server mysql-client php5 libapache2-mod-php5 phpmyadmin zip unzip -y
 echo "Downloading and extracting nconf . . . . "
 wget http://sourceforge.net/projects/nconf/files/nconf/1.3.0-0/nconf-1.3.0-0.tgz
-apache_dir=`grep -i 'DocumentRoot' /etc/apache2/sites-available/default | awk '{print $2}'`
+apache_dir=`cat /etc/apache2/sites-{enabled,available}/000-default.conf | grep -i 'DocumentRoot' | awk '{print $2}' | head -1 2>error`
 nconfdir="$apache_dir/nconf"
 tar xvzf nconf-1.3.0-0.tgz -C $apache_dir
 chown -R www-data:www-data $nconfdir
@@ -25,7 +25,6 @@ cd $apache_dir/nconf
 patch -p0 --verbose < nconf-1.3.0-0_not_used_advanced_services_wont_be_written.patch
 echo "Creating Database for nconf . . . ."
 mysql -uroot -pmysql < ~/nagios/create_database.sql
-mysql -unconf -pnconf < INSTALL/create_database.sql
 cp -rfv /usr/local/nagios/etc/nagios.cfg /usr/local/nagios/etc/nagios.cfg.orig
 cp -rfv /usr/local/nagios/etc/nagios.cfg $nconfdir/static_cfg/
 sed -i 's/^cfg_file/#cfg_file/g' $nconfdir/static_cfg/nagios.cfg
@@ -40,22 +39,11 @@ cp -dpR $nconfdir/config.orig/* $nconfdir/config
 sed -ie "10s/NConf/nconf/g" $nconfdir/config/mysql.php
 sed -ie "12s/link2db/nconf/g" $nconfdir/config/mysql.php
 sed -ie "11s/^/#/g" $nconfdir/config/nconf.php
-sed -ie "16s/^/define('NCONFDIR', \"\/var\/www\/nconf\")\;/g" $nconfdir/config/nconf.php
-sed -ie "23s/\/var\/www\/nconf\/bin\/nagios/$nconfdir\/bin\/nagios/g" $nconfdir/config/nconf.php
-sed -ie "32s/^/define\(\'CHECK_STATIC_SYNTAX\', 0\);\n/" $nconfdir/config/nconf.php
-#rm -rfv INSTALL INSTALL.php UPDATE UPDATE.php
+sed -ie "16s|^|define('NCONFDIR', \"$nconfdir\")\;|g" $nconfdir/config/nconf.php
+sed -ie "23s|/var/www/nconf/bin/nagios|/usr/local/nagios/bin/nagios|g" $nconfdir/config/nconf.php
+#sed -ie "32s/^/define\(\'CHECK_STATIC_SYNTAX\', 0\);\n/" $nconfdir/config/nconf.php
+rm -rfv INSTALL INSTALL.php UPDATE UPDATE.php
 cp -R $nconfdir/img /usr/local/nagios/share/images
-/etc/init.d/nagios reload
-#cat > /var/www/html/nconf/config/mysql.php <<EOM
-#<?php
-
-#define('DBHOST', 'localhost');
-#define('DBNAME', 'nconf');
-#define('DBUSER', 'nconf');
-#define('DBPASS', 'nconf');
-#
-#?>
-#EOM
-
+mysql -unconf -pnconf nconf < INSTALL/create_database.sql
 /etc/init.d/nagios reload
 
