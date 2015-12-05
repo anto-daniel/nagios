931aa2e21c1ef3fedd6960f2cdeb1e23f2ea7c50
Anto Daniel <antodaniel@inmobi.com>
2015-12-05 14:00:30 +0000

    modifying nrpe installation

--- nrpe_installation.sh
+++ nrpe_installation.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 
-apt-get update && apt-get install gcc gawk openssl make libssl-dev -y
+apt-get update && apt-get install gcc gawk openssl make libssl-dev expect -y
+echo "Downloading nrpe plugin . . ."
 cd /tmp
-wget http://nagios-plugins.org/download/nagios-plugins-2.0.3.tar.gz
+np_cmd=`wget http://nagios-plugins.org/download/nagios-plugins-2.0.3.tar.gz 2> /tmp/np_log`
 d_st=`echo $?`
 if [[ $d_st -ne 0 ]]; then
   echo "Unable to download nagios plugin. Exiting...."
   exit 1
 fi
+tar xvzf nagios-plugins-2.0.3.tar.gz
 cd nagios-plugins-2.0.3
 ./configure --with-nagios-user=nagios --with-nagios-group=nagios --enable-perl-modules --enable-extra-opts
 make
@@ -33,8 +35,8 @@ EOM
   chmod u+x nagios_passwd.sh
   ./nagios_passwd.sh
 fi
-
-wget http://downloads.sourceforge.net/project/nagios/nrpe-2.x/nrpe-2.15/nrpe-2.15.tar.gz
+echo "Downloading nrpe package . . ."
+nconf_cmd=`wget http://downloads.sourceforge.net/project/nagios/nrpe-2.x/nrpe-2.15/nrpe-2.15.tar.gz 2> /tmp/nconf_log`
 n_st=`echo $?`
 if [[ $n_st -ne 0 ]]; then
  echo "Unable to download nrpe. Exiting . . ."
@@ -42,7 +44,7 @@ if [[ $n_st -ne 0 ]]; then
 fi 
 tar xvzf nrpe-2.15.tar.gz
 cd nrpe-2.15
-./configure --with-ssl=/usr/bin/openssl --with-ssl-lib=/usr/lib/x86_64-linux-gnu
+./configure --with-ssl=/usr/bin/openssl --with-ssl-lib=/usr/lib/x86_64-linux-gnu --enable-command-args
 make all
 make install-plugin
 make install-daemon
